2e881e1f3c3a39314dae69aaddc3cf3bdc9c4ce5
calpis <calvinlyn11@gmail.com>
2023-09-03 05:12:54 +0800

    Minor errors

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("1. Decimal To Binary")
+	golang.decimalToBinary()
+	fmt.Println("2. Binary To Decimal")
+	golang.binaryToDecimal()
+
+	fmt.Println("1. Bubble Sort")
+	golang.bubbleSort()
+}
