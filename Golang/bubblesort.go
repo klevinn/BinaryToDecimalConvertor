308e773c53acd5b7c22b71366b69da6a36403121
calpis <calvinlyn11@gmail.com>
2023-09-03 05:46:08 +0800

    Fixing import errors

--- Golang/bubblesort.go
+++ Golang/bubblesort.go
@@ -18,3 +18,18 @@ func bubbleSort() {
 	}
 	fmt.Println("Sorted Array:", arr)
 }
+
+func revBubbleSort() {
+	arr := [10]int{5, 12, 42, 1, 5, 7, 8} // declare array
+	n := len(arr)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-1; j++ {
+			if arr[i] < arr[i+1] {
+				store := arr[i]
+				arr[i] = arr[i+1]
+				arr[i+1] = store
+			}
+		}
+	}
+	fmt.Println("Reverse Sorted Array:", arr)
+}
--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
+	// "github.com/klevinn/Golang-Projects/Golang"
 )
 
 func main() {
 	fmt.Println("1. Decimal To Binary")
-	Golang.decimalToBinary()
+	// Golang.bubbleSort()
 	fmt.Println("2. Binary To Decimal")
-	Golang.binaryToDecimal()
 
 	fmt.Println("1. Bubble Sort")
-	Golang.bubbleSort()
 }
